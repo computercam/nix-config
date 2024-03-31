a751bed6a7d9bb1ead7c5d549b4ca9f70a0c47c8
cameron <csanders@protonmail.com>
2024-03-30 19:23:01 -0500

    glitchtip secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,14 @@
 let 
   main = builtins.readFile ../_/id_rsa.pub;
   borgbackup = builtins.readFile ../_/id_rsa.borgbackup.pub;
+  glitchtip = builtins.readFile ../_/id_rsa.glitchtip.pub
 in {
   "cf_account_id".publicKeys = [ main ];
   "cf_api_token".publicKeys = [ main ];
   "cf_tunnel_password".publicKeys = [ main ];
   "cf_zone_id".publicKeys = [ main ];
   "borgbackup".publicKeys = [ borgbackup ];
+  "glitchtip_key".publicKeys = [ glitchtip ];
   "nextcloud_mysql_password".publicKeys = [ main ];
   "nextcloud_mysql_root_password".publicKeys = [ main ];
   "vscode_hashed_password".publicKeys = [ main ];
